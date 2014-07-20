4a70846d6eaacb7766c6859f09390d05c8570a78
Thomas Giles <wthit56@googlemail.com>
2014-07-20 18:03:03 +0100

    Fixed prediction
    Increased demo simulation's accuracy

--- Phys.js
+++ Phys.js
@@ -1,10 +1,10 @@
 /*jshint strict: false */
 /*global console */
 
-var Phys = (function(){
+var Phys = (function(test) {
 	"use strict";
 
-	function Phys(position, velocity, acceleration){
+	function Phys(position, velocity, acceleration) {
 		this.set.apply(this,arguments);
 	}
 	Phys.prototype = {
@@ -36,88 +36,71 @@ var Phys = (function(){
 	
 	Phys.predict = {
 		time: {
-			byPosition: function(phys, position) {
-				// p = ((v + (v + a * t)) / 2) * t;
-				// p = (tv + tv + att) / 2
-				// p = (2tv + att) / 2
-				// p = tv + (att / 2)
-				// tv + (att / 2) - p = 0
-				// (a / 2) t^2 + v t + -p
-				// (-v +- sqrt(-v*-v - 4*(a/2)*(-p))) / 2(a/2)
-				var p=phys.position, v=phys.velocity, a=phys.acceleration;
-				if(a){
-					// p = delta position
-					// t = delta time
-					// (a / 2)(t ^ 2) + (v * t) - p
+			byPosition: (function() {
+				var p, v, a;
+				var sqrt, $1, $2;
+				return function Phys_predict_time_byPosition(phys, position) {
+					p = phys.position; v = phys.velocity; a = phys.acceleration;
+					
+					if (
+						((position > p) && (v <= 0) && (a <= 0)) ||
+						((position < p) && (v >= 0) && (a >= 0))
+					) { }
+					else if (a) {
+						sqrt = Math.sqrt(
+							(v * v)
+							- (2 * a * (p - position))
+						);
 
-					// (-B +- sqrt(B^2 - 4AC)) / 2AC
-					// (-v +- sqrt(v^2 - 4(a/2)(-p))) / 2(a/2)
-
-					//console.log(A+"(t^2) + "+B+"t + "+C);
-
-					return (
-						(-v +
-							(((a < 0) ? -1 : 1) * Math.sqrt(
-								(v * v) - (2 * a * (phys.position - position))
-							))
-						)
-						/ a
-					);
-
-					/*var A = a/2, B = v, C = phys.position - position;
-					return (
-						(-B + ((A<0?-1:1) * Math.sqrt((B*B)  - (4 * A * C))))
-						/ (2 * A)
-					);*/
-				}
-				else if(v){
-					if(position!==p){
-						if(position>p){
-							if(v>0){return ((position-p)/v);}
-						}
-						else{
-							if(v<0){return ((position-p)/v);}
-						}
+						$1 = (-v + sqrt) / a; $2 = (-v - sqrt) / a;
+						return (($1 > $2) ? $1 : $2);
 					}
-				}
-			}
+					else if (v) {
+						return ((position - p) / v);
+					}
+					
+					return null;
+				};
+			})()
 		}
 	};
-	
-	
-	console.groupCollapsed("Phys.predict.time.byPosition");
-		function test(position, velocity, acceleration, newPosition, expected) {
-			var phys = new Phys(position, velocity, acceleration);
-			var result = Phys.predict.time.byPosition(phys, newPosition);
-			var success = (result.toFixed(10) === expected.toFixed(10));
-			console.group("Phys.predict.time.byPosition(new Phys(" + position + ", " + velocity + ", " + acceleration + "), " + newPosition + ")");
-				console[success ? "log" : "error"](
-					"==", result,
-					"expected", expected
-				);
-				var decimals = 13;
-				console.log(
-					"simulation check:", phys.simulate(result).position,
-					"expected:", newPosition
-				);
-			console.groupEnd();
-		}
-
-		// https://www.khanacademy.org/science/physics/two-dimensional-motion/two-dimensional-projectile-mot/v/launching-and-landing-on-different-elevations
-		test(25, 90 * Math.sin(53 * (Math.PI / 180)), -9.8, 9, 14.888138154688914);
-		test(0, 1, 0, 5, 5);
-		test(0, 1, 1, 17.5, 5);
-	
-	
-		function throwBall(t) {
-			var v0 = (t/2) * -9.8 * -1
-			test(0, v0, -9.8, 0, t);
-		}
-		for(var i=0; i<10; i++){
-			throwBall(Math.random()*100);
-		}
-	
-	console.groupEnd();
-	
+		
 	return Phys;
 })();
+
+
+
+function test() {
+	function testPrediction(position, velocity, acceleration, newPosition, expected) {
+		var phys = new Phys(position, velocity, acceleration);
+		var result = Phys.predict.time.byPosition(phys, newPosition);
+		var success = (result.toFixed(10) === expected.toFixed(10));
+		console.group("Phys.predict.time.byPosition(new Phys(" + position + ", " + velocity + ", " + acceleration + "), " + newPosition + ")");
+			console[success ? "log" : "error"](
+				"==", result,
+				"expected", expected
+			);
+			var decimals = 13;
+			console.log(
+				"simulation check:", phys.simulate(result).position,
+				"expected:", newPosition
+			);
+		console.groupEnd();
+	}
+	testPrediction.throwBall = function(t) {
+		var v0 = (t/2) * -9.8 * -1
+		testPrediction(0, v0, -9.8, 0, t);
+	}
+	
+	console.groupCollapsed("Phys.predict.time.byPosition");
+		// https://www.khanacademy.org/science/physics/two-dimensional-motion/two-dimensional-projectile-mot/v/launching-and-landing-on-different-elevations
+		testPrediction(25, 90 * Math.sin(53 * (Math.PI / 180)), -9.8, 9, 14.888138154688914);
+		testPrediction(0, 1, 0, 5, 5);
+		testPrediction(0, 1, 1, 17.5, 5);
+
+		for(var i=0; i<10; i++){
+			testPrediciton.throwBall(Math.random()*100);
+		}
+	console.groupEnd();
+}
+//test();
