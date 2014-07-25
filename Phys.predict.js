603d9bb617a55ee47694daaf9df0e54869c03558
Thomas Giles <wthit56@googlemail.com>
2014-07-25 20:49:15 +0100

    time.byPosition accuracy

--- Phys.predict.js
+++ Phys.predict.js
@@ -2,7 +2,7 @@ if(!Phys){var Phys = {};}
 Phys.predict = {
     time: {
         byPosition: (function() {
-            var p, v, a, tosqrt, sqrt,
+            var p, pd, v, a, tosqrt, sqrt,
 				$1, $2;
 
             return function Phys_predict_time_byPosition(phys, position) {
@@ -11,23 +11,40 @@ Phys.predict = {
                 if (
                     ((position > p) && (v <= 0) && (a <= 0)) ||
                     ((position < p) && (v >= 0) && (a >= 0))
-                ) {
-					return null;
-				}
+                ) { return null; }
                 else if (a) {
-                    tosqrt = ((v * v) - (2 * a * (p - position)));
-                    sqrt = (tosqrt === 0) ? 0 : Math.sqrt(
-                        tosqrt < 0 ? -tosqrt : tosqrt
-                    );
-					
-					$1 = (-v + sqrt) / a, $2 = (-v - sqrt) / a;
-					return (
-						($1 <= 0)
-							? ($2 <= 0) ? null : $2
-							: ($2 <= 0)
-								? $1
-								: ($1 < $2) ? $1 : $2
-					);
+					if(p===position){
+						sqrt = (v < 0) ? -v : v;
+						
+						return (
+							((a < 0) === (v < 0))
+								? null
+								: (a<0)
+									? (-v - sqrt) / a
+									: (-v + sqrt) / a
+						);
+					}
+					else{
+						tosqrt = ((v * v) - (2 * a * (p - position)));
+						
+						sqrt = (
+							(tosqrt === 0)
+							? 0
+							: Math.sqrt(
+								tosqrt < 0 ? -tosqrt : tosqrt
+							)
+						);
+						
+						$1 = (-v + sqrt) / a, $2 = (-v - sqrt) / a;
+						
+						return (
+							($1 <= 0)
+								? ($2 <= 0) ? null : $2
+								: ($2 <= 0)
+									? $1
+									: ($1 < $2) ? $1 : $2
+						);
+					}
                 }
                 else if (v) {
                     return ((position - p) / v);
